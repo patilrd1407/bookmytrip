f54d1ad5b7e6bc1661134e68b66725a834fa57fa
Rohan Patil <patilrd1407@gmail.com>
2026-05-18 09:37:52 +0530

    codeRefactor

---
+++ src/main/java/com/example/bookmytrip/BookmytripApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmytripApplication.class, args);
+	}
+
+}
